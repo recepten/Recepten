abad25c83aaafd9ed72ce8fee13898eb5f217d51
regilioDielemans <regilio_dielemans@hotmail.com>
2016-05-17 14:10:24 +0200

    laravel

---
+++ recepten/app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('recepten');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('recepten', function () {
+    return view('recepten');
+});
+
+Route::get('recept', function () {
+    return view('recepten');
+});

ac1e4af239cc9df938b882689f6eae060d09f04a
regilioDielemans <regilio_dielemans@hotmail.com>
2016-05-18 15:23:29 +0200

    recepten

--- recepten/resources/views/recept.blade.php
+++ recepten/resources/views/recept.blade.php
@@ -5,7 +5,9 @@
 
 	@foreach($recepten as $recept)
 		<h5>{{ $recept->titel }}</h5>
+		<p>{{$recept->beschrijving}}</p>
+		<p>{{$recept->ingredienten}}</p>
+
 		<p>{{$recept->upvotes}}</p>
 	@endforeach
 @endsection
-
---
+++ recepten/resources/views/templates/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/main.css">
+		<link rel="stylesheet" href="/css/material-kit.css">
+
+	</head>
+	<body>
+	 	<div class="container">
+	        @yield('content')
+    	</div>
+    </body>
+</html>

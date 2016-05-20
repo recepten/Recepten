6cd6f2754f30951f22557213df944e0b4a9aedcd
Rick <RickVerheijen1@hotmail.com>
2016-05-20 10:57:03 +0200

    in container gezet

--- recepten/resources/views/recepten.blade.php
+++ recepten/resources/views/recepten.blade.php
@@ -2,10 +2,11 @@
 
 
 @section('content')
-
+<div class="container">
 	@foreach($recepten as $recept)
 		<h5>{{ $recept->titel }}</h5>
 		<p>{{$recept->upvotes}}</p>
 	@endforeach
+</div>
 @endsection
 

020b111de7fae7ddd3b48b270dbd5af3e30c24bb
Rick <RickVerheijen1@hotmail.com>
2016-05-23 15:21:53 +0200

    css

--- recepten/resources/views/recepten.blade.php
+++ recepten/resources/views/recepten.blade.php
@@ -6,8 +6,13 @@
 
 	@foreach($recepten as $recept)
 	<div class="recept col-md-12">
-		<h5>{{ $recept->titel }}</h5>
-		<p>{{$recept->upvotes}}</p>
+	<div class="left">
+		<div class="foto">(foto)</div>
+	</div>
+		<div class="right">
+			<h5 class="title">{{ $recept->titel }}</h5>
+		<p class="upvotes">{{$recept->upvotes}}</p>
+		</div>
 	</div>
 	@endforeach
 

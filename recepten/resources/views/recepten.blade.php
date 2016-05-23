b7be410d0edb3bd7f63f71433931f5bd78d49beb
Rick <RickVerheijen1@hotmail.com>
2016-05-23 14:37:03 +0200

    css

--- recepten/resources/views/recepten.blade.php
+++ recepten/resources/views/recepten.blade.php
@@ -3,10 +3,12 @@
 
 @section('content')
 <div class="container">
+<div class="recept col-md-12">
 	@foreach($recepten as $recept)
 		<h5>{{ $recept->titel }}</h5>
 		<p>{{$recept->upvotes}}</p>
 	@endforeach
+</div>
 </div>
 @endsection
 

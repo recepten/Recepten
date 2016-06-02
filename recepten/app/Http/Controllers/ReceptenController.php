693cabca0840ce82ead7613a49dff30955770fd2
Rick <RickVerheijen1@hotmail.com>
2016-06-02 12:24:12 +0200

    upload image

--- recepten/app/Http/Controllers/ReceptenController.php
+++ recepten/app/Http/Controllers/ReceptenController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Company;
 use App\Auth;
-use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 

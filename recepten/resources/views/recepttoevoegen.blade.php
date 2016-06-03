a8579a50df4b05b6d3084fcea64a9172051415b6
Rick <RickVerheijen1@hotmail.com>
2016-06-03 15:31:00 +0200

    ingredienten

--- recepten/resources/views/recepttoevoegen.blade.php
+++ recepten/resources/views/recepttoevoegen.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<form action="{{ url('/recept/opslaan') }}" enctype="multipart/form-data" method="POST" class="form-horizontal">
+<form action="{{ url('/recept/opslaan') }}" enctype="multipart/form-data" method="POST" class="form-horizontal" id="surveyForm">
 
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
@@ -47,10 +47,23 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="Ingredienten">Ingredienten</label>
   <div class="col-md-4">
-  <input id="Ingredienten" name="Ingredienten" type="text" placeholder="Ingredienten" class="form-control input-md" required="">
+  <input id="Ingredienten" name="option[]" type="text" placeholder="Ingredienten" class="form-control input-md" required="">
 
   </div>
+  <div class="col-xs-4">
+            <button type="button" class="btn btn-default addButton"><i class="fa fa-plus"></i></button>
+        </div>
 </div>
+
+<!-- The option field template containing an option field and a Remove button -->
+    <div class="form-group hide" id="optionTemplate">
+        <div class="col-xs-offset-3 col-xs-5">
+            <input class="form-control" type="text" name="option[]" />
+        </div>
+        <div class="col-xs-4">
+            <button type="button" class="btn btn-default removeButton"><i class="fa fa-minus"></i></button>
+        </div>
+    </div>
 
 <div class="form-group">
 <label class="col-md-4 control-label" for="foto">foto</label>
@@ -61,13 +74,104 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="Toevoegen"></label>
   <div class="col-md-4">
-    <button id="Toevoegen" name="Toevoegen" class="btn btn-primary">Recept toevoegen</button>
+    <button id="Toevoegen" name="Toevoegen" type="submit" class="btn btn-default">Recept toevoegen</button>
   </div>
 </div>
 
 </fieldset>
 </form>
 
+<?php
 
+$array = array( 1, 2, 3 );
+$string = serialize( $array );
+echo $string;
+
+?>
+
+<script>
+$(document).ready(function() {
+    // The maximum number of options
+    var MAX_OPTIONS = 25;
+
+    $('#surveyForm')
+        .formValidation({
+            framework: 'bootstrap',
+            icon: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                question: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The question required and cannot be empty'
+                        }
+                    }
+                },
+                'option[]': {
+                    validators: {
+                        notEmpty: {
+                            message: 'The option required and cannot be empty'
+                        },
+                        stringLength: {
+                            max: 100,
+                            message: 'The option must be less than 100 characters long'
+                        }
+                    }
+                }
+            }
+        })
+
+        // Add button click handler
+        .on('click', '.addButton', function() {
+            var $template = $('#optionTemplate'),
+                $clone    = $template
+                                .clone()
+                                .removeClass('hide')
+                                .removeAttr('id')
+                                .insertBefore($template),
+                $option   = $clone.find('[name="option[]"]');
+
+            // Add new field
+            $('#surveyForm').formValidation('addField', $option);
+        })
+
+        // Remove button click handler
+        .on('click', '.removeButton', function() {
+            var $row    = $(this).parents('.form-group'),
+                $option = $row.find('[name="option[]"]');
+
+            // Remove element containing the option
+            $row.remove();
+
+            // Remove field
+            $('#surveyForm').formValidation('removeField', $option);
+        })
+
+        // Called after adding new field
+        .on('added.field.fv', function(e, data) {
+            // data.field   --> The field name
+            // data.element --> The new field element
+            // data.options --> The new field options
+
+            if (data.field === 'option[]') {
+                if ($('#surveyForm').find(':visible[name="option[]"]').length >= MAX_OPTIONS) {
+                    $('#surveyForm').find('.addButton').attr('disabled', 'disabled');
+                }
+            }
+        })
+
+        // Called after removing the field
+        .on('removed.field.fv', function(e, data) {
+           if (data.field === 'option[]') {
+                if ($('#surveyForm').find(':visible[name="option[]"]').length < MAX_OPTIONS) {
+                    $('#surveyForm').find('.addButton').removeAttr('disabled');
+                }
+            }
+        });
+});
+</script>
 
 @endsection
--- recepten/resources/views/templates/default.blade.php
+++ recepten/resources/views/templates/default.blade.php
@@ -4,10 +4,14 @@
 		<meta charset="UTF-8">
 		<title>Recepten</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 		<link rel="stylesheet" href="/css/main.css">
     <link href='https://fonts.googleapis.com/css?family=Kalam' rel='stylesheet' type='text/css'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/formvalidation/0.6.1/js/formValidation.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/formvalidation/0.6.1/js/framework/bootstrap.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 
